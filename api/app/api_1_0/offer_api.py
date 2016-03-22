7b520803bb606eb9ec4558731f7cb5b212f49aa4
Amit Saraswat <amit5148@gmail.com>
2016-03-22 21:13:04 +0530

    order api v0.1

--- api/app/api_1_0/offer_api.py
+++ api/app/api_1_0/offer_api.py
@@ -1,39 +1,39 @@
-# import logging
-# from datetime import datetime
+import logging
+from datetime import datetime
 
-# from flask import  request
-# import ujson
-# from app.api_1_0 import api
-# from app.decorator import json
-# from app.src.offer import Offer
+from flask import  request
+import ujson
+from app.api_1_0 import api
+from app.decorator import json
+from app.src.offer import Offer
 
-# logger = logging.getLogger()
+logger = logging.getLogger()
 
-# @api.route("/offer/<pageno>/<pagesize>", methods=["GET"])
-# @json
-# def get_offer_list(pageno, pagesize):
-#     result = Offer().get_list()
-#     return result
+@api.route("/offer/<pageno>/<pagesize>", methods=["GET"])
+@json
+def get_offer_list(pageno, pagesize):
+    result = Offer().get_list()
+    return result
 
-# @api.route("/offer/<offer_id>", methods=["GET"])
-# @json
-# def get_offer_by_id(offer_id):
-#     cat = Offer(offer_id)
-#     return cat.get()
+@api.route("/offer/<offer_id>", methods=["GET"])
+@json
+def get_offer_by_id(offer_id):
+    result = Offer(offer_id)
+    return result.get()
 
 
-# @api.route("/offer", methods=["POST"])
-# @json
-# def create_offer():
-#     logger.debug(request.data)
-#     return Offer().create(ujson.loads(request.data))
+@api.route("/offer", methods=["POST"])
+@json
+def create_offer():
+    logger.debug(request.data)
+    return Offer().create(ujson.loads(request.data))
 
-# @api.route("/offer", methods=["PUT"])
-# @json
-# def update_offer():
-#     logger.debug(request.data)
-#     data = ujson.loads(request.data)
-#     id = data["id"]
-#     del(data["id"])
-#     return Offer(id).update(data)
+@api.route("/offer", methods=["PUT"])
+@json
+def update_offer():
+    logger.debug(request.data)
+    data = ujson.loads(request.data)
+    id = data["id"]
+    del(data["id"])
+    return Offer(id).update(data)
 
