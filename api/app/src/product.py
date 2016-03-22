871117ff6a2b70731e36b9472a2361b33f39d7f9
Amit Saraswat <amit5148@gmail.com>
2016-03-22 23:40:51 +0530

    product api v0.0.1

--- api/app/src/product.py
+++ api/app/src/product.py
@@ -1,4 +1,5 @@
 import logging
+from sqlalchemydb import AlchemyDB
 from base_catalog import BaseCatalog
 
 logger = logging.getLogger()
@@ -8,4 +9,17 @@ class Product(BaseCatalog):
 
     def __init__(self, id=None):
         BaseCatalog.__init__(self, "product", id)
+
+    def get_attribute_values(self):
+        db = AlchemyDB()
+        product_attribute_values = db.find("product_attribute_value", product_id=self.id)
+        logger.debug(product_attribute_values)
+        attribute_ids = list(set([pav['attribute_id'] for pav in product_attribute_values]))
+        attributes = db.find("attribute", id=attribute_ids)
+        attribute_store = {at['id']:at for at in attributes}
+        logger.debug(attribute_store)
+        for pav in product_attribute_values:
+            pav['attribute'] = attribute_store[pav['attribute_id']]
+            pav['value'] = db.find_one(pav['attribute']['value_type']+'_value', id=pav['value_id'])
+        return product_attribute_values
 
