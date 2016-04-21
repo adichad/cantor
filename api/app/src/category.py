6d4145b0583660deb2feb35e69157e8403b9efe1
Amit Saraswat <amit5148@gmail.com>
2016-04-21 19:20:12 +0530

    changes to restrict creation of existing category attribute mapping

--- api/app/src/category.py
+++ api/app/src/category.py
@@ -14,9 +14,12 @@ class Category(BaseCatalog):
         db = AlchemyDB()
         insert = []
         for item in data:
-            item["category_id"] = self.id
-            insert.append(item)
-        db.insert_row_batch('category_attribute', insert)
+            existing_mappings = db.find('category_attribute', category_id=self.id, attribute_id=item["attribute_id"])
+            if len(existing_mappings) == 0:
+                item["category_id"] = self.id
+                insert.append(item)
+        if len(insert):
+            db.insert_row_batch('category_attribute', insert)
         return self.get_category_attribute_map()
 
     def get_category_attribute_map(self):
